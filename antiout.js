0b889624301c4e4a3712717864f3824d32e2b68b
DaoNhatAnh <79800271+DaoNhatAnh@users.noreply.github.com>
2021-10-09 21:32:53 +0700

    Update antiout.js

--- antiout.js
+++ antiout.js
@@ -1,27 +1,22 @@
-module.exports.config = {
+module.exports.config = {
 	name: "antiout",
 	version: "1.0.0",
-	credits: "ProCoderMew - ĐNA fix it",
+	credits: "DungUwU (Khánh Milo Fix)",
 	hasPermssion: 1,
-	description: "Tắt/Bật Antiout để add lại thành viên out chùa",
-	usages: "on/off",
-    commandCategory: "other",
+	description: "Bật tắt antiout",
+	usages: "antiout on/off",
+    commandCategory: "system",
 	cooldowns: 0
 };
 
-module.exports.run = async function({ api, event }) {
-    const { writeFileSync } = global.nodemodule["fs-extra"];
-    const { resolve } = global.nodemodule["path"];
-    const path = resolve(__dirname, 'cache', 'meewmeew.json');
-    const { threadID, messageID } = event;
-    const database = require(path);
-    const { antiout } = database;
-    if (antiout[threadID] == true) {
-        antiout[threadID] = false;
-        api.sendMessage("Đã tắt chế độ chống out chùa.", threadID, messageID);
-    } else {
-        antiout[threadID] = true;
-        api.sendMessage("Đã bật chế độ chống out chùa. (Không chắc là có thể add lại được toàn bộ thành viên out chùa)", threadID, messageID);
-    }
-    writeFileSync(path, JSON.stringify(database, null, 4));
+module.exports.run = async({ api, event, Threads}) => {
+	let data = (await Threads.getData(event.threadID)).data || {};
+    if (typeof data["antiout"] == "undefined" || data["antiout"] == false) data["antiout"] = true;
+	else data["antiout"] = false;
+	
+	await Threads.setData(event.threadID, { data });
+	global.data.threadData.set(parseInt(event.threadID), data);
+	
+	return api.sendMessage(`Đã ${(data["antiout"] == true) ? "bật" : "tắt"} thành công antiout!`, event.threadID);
+
 }
